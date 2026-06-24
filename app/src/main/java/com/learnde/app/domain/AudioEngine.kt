6fa518ffbeb14f37438b1ee2f7824d25d9253e88
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-06-25 01:00:45 +0200

    [Pipeline] CREATE AudioEngine.kt

---
+++ app/src/main/java/com/learnde/app/domain/AudioEngine.kt
@@ -0,0 +1,29 @@
+package com.learnde.app.domain
+
+import kotlinx.coroutines.flow.Flow
+
+interface AudioEngine {
+
+    val micOutput: Flow<ByteArray>
+    val isCapturing: Boolean
+    val isPlaying: Boolean
+    val playbackAudibleUntilMs: Long
+
+    suspend fun startCapture()
+    suspend fun stopCapture()
+    suspend fun enqueuePlayback(pcmData: ByteArray)
+    suspend fun flushPlayback()
+    suspend fun onTurnComplete()
+    suspend fun initPlayback()
+    suspend fun releaseAll()
+
+    fun updateJitterConfig(preBufferChunks: Int, timeoutMs: Long, queueCapacity: Int)
+
+    val playbackSync: Flow<ByteArray>
+
+    fun setPlaybackVolume(gain: Float)
+
+    fun setMicGain(gain: Float)
+
+    fun setSpeakerRouting(forceSpeaker: Boolean)
+}
