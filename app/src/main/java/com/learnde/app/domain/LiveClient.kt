4562cad942ce1a66d82134e4cce0a5bbb33cb24e
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-06-25 01:00:07 +0200

    [Pipeline] CREATE LiveClient.kt

---
+++ app/src/main/java/com/learnde/app/domain/LiveClient.kt
@@ -0,0 +1,38 @@
+package com.learnde.app.domain
+
+import com.learnde.app.domain.model.ConversationMessage
+import com.learnde.app.domain.model.GeminiEvent
+import com.learnde.app.domain.model.SessionConfig
+import kotlinx.coroutines.flow.Flow
+
+interface LiveClient {
+
+    val events: Flow<GeminiEvent>
+    val sessionHandle: String?
+
+    val isReady: Boolean
+
+    suspend fun connect(apiKey: String, config: SessionConfig, logRaw: Boolean = false)
+
+    fun sendAudio(pcmData: ByteArray)
+
+    fun sendText(text: String)
+
+    fun sendRealtimeText(text: String)
+
+    fun sendVideoFrame(jpegBytes: ByteArray)
+
+    fun sendAudioStreamEnd()
+
+    fun sendToolResponse(responses: List<ToolResponse>)
+
+    fun restoreContext(history: List<ConversationMessage>)
+
+    suspend fun disconnect()
+}
+
+data class ToolResponse(
+    val name: String,
+    val id: String,
+    val result: String
+)
