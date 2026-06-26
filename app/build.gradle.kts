06ce94a58bf8675b3485662f09ed0215eb31c355
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-06-26 17:09:59 +0200

    [Pipeline] build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,6 +10,7 @@
 // ═══════════════════════════════════════════════════════════
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
     id("org.jetbrains.kotlin.plugin.serialization")
     id("org.jetbrains.kotlin.plugin.compose")
     id("com.google.devtools.ksp")
