bb8afa01216de0ade5a4d4ec28f1b55dd75fb271
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-06-26 17:10:00 +0200

    [Pipeline] GeminiLiveApplication.kt

--- app/src/main/java/com/learnde/app/GeminiLiveApplication.kt
+++ app/src/main/java/com/learnde/app/GeminiLiveApplication.kt
@@ -4,4 +4,11 @@ import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class GeminiLiveApplication : Application()
+class GeminiLiveApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        if (BuildConfig.DEBUG) {
+            timber.log.Timber.plant(timber.log.Timber.DebugTree())
+        }
+    }
+}
