23a36533c8b14bfa4a132df71239124c3e4e52f9
rdumbrovskyimail-eng <r.dumbrovskyi.mail@gmail.com>
2026-07-01 01:52:42 +0200

    [Pipeline] ClientViewModel.kt

--- app/src/main/java/com/learnde/app/presentation/client/ClientViewModel.kt
+++ app/src/main/java/com/learnde/app/presentation/client/ClientViewModel.kt
@@ -56,6 +56,9 @@ class ClientViewModel @Inject constructor(
     fun sendText(text: String, attachments: List<android.net.Uri> = emptyList()) = session.sendText(text, attachments)
     fun toggleMic() = session.toggleMic()
     fun clearDashboard() = session.clearDashboard()
+    fun playPronunciation(item: com.learnde.app.domain.model.PronunciationItem) =
+        session.playPronunciation(item)
+    fun clearPronunciations() = session.clearPronunciations()
     fun clearError() = session.clearError()
     fun retry() = session.retry()
 
